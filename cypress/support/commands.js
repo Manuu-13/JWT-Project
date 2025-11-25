c73202800baede5b2b6a31c89063fc503ba64462
Akanksha Veer <akankshaveer28@gmail.com>
2025-11-25 18:07:13 +0530

    update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -47,59 +47,43 @@ Cypress.Commands.add('loginToSalesforceJWT', (username) => {
   });
 });
 //login Through API
-// Cypress.Commands.add("loginToSalesforceAPI", () => {
-//   const authUrl = "https://login.salesforce.com/services/oauth2/authorize";
+// import testData from "../fixtures/salesforceTestData.json";
+// Cypress.Commands.add("navigateAndLoginSalesforce", (username, password) => {
+//   const requestBody = testData.requestBodyForSalesforceLogin;
+//   requestBody.un=username;
+//   requestBody.pw=password;
+//   cy.session("SalesforceLogin", () => {
+//     cy.request({
 
-//   cy.task("generatePKCECodes").then(({ code_verifier, code_challenge }) => {
-    
-//     const params = new URLSearchParams({
-//       response_type: "code",
-//       client_id: Cypress.env("SF_CLIENT_ID"),    // External App Client ID
-//       redirect_uri: Cypress.env("SF_REDIRECT_URI"), // Must match registered URI
-//       code_challenge_method: "S256",
-//       code_challenge,
-//       scope: "openid api refresh_token", // Optional but recommended
-//     });
+//       body: requestBody,
+//       form: true,
+//       method: "POST",
+//       url: "https://login.salesforce.com/",
+//     }).then((response) => {
+//   cy.wrap(response.body.access_token).as("accessToken");
+//   cy.wrap(response.body.instance_url).as("instanceUrl");
+// });
+//   });
 
-//     // 1️⃣ Open Salesforce Login Page
-//     cy.visit(`${authUrl}?${params.toString()}`);
+//   cy.visit('https://myridius16-dev-ed.develop.lightning.force.com/lightning/page/home');
+// });
 
-//     // 2️⃣ Enter Login Credentials
-//     cy.get('input#username').type(Cypress.env("SF_USERNAME"));
-//     cy.get('input#password').type(Cypress.env("SF_PASSWORD"), { log: false });
-//     cy.get('#Login').click();
 
-//     // 3️⃣ Wait for redirect URL with code
-//     cy.url().should("contain", "code=");
 
-//     cy.url().then((url) => {
-//       const auth_code = new URL(url).searchParams.get("code");
 
-//       // 4️⃣ Exchange code for access token
-//       cy.request({
-//         method: "POST",
-//         url: "https://login.salesforce.com/services/oauth2/token",
-//         form: true,
-//         body: {
-//           grant_type: "authorization_code",
-//           client_id: Cypress.env("SF_CLIENT_ID"),
-//           redirect_uri: Cypress.env("SF_REDIRECT_URI"),
-//           code: auth_code,
-//           code_verifier,
-//         },
-//       }).then((response) => {
-//         // 5️⃣ Set session token
-//         const accessToken = response.body.access_token;
+// Cypress.Commands.add("navigateAndLoginSalesforce", (username, password) => {
+//   const requestBody = testData.requestBodyForSalesforceLogin;
+//   requestBody.un=username;
+//   requestBody.pw=password;
+//   cy.session("SalesforceLogin", () => {
+//     cy.request({
 
-//         cy.setCookie("sid", accessToken, {
-//           domain: ".develop.lightning.force.com",
-//           secure: true,
-//           httpOnly: false, // Allows Cypress to use session
-//         });
-
-//         // 6️⃣ Visit Salesforce home
-//         cy.visit("https://myridius16-dev-ed.develop.lightning.force.com/lightning/page/home");
-//       });
+//       body: requestBody,
+//       form: true,
+//       method: "POST",
+//       url: "https://login.salesforce.com/",
 //     });
 //   });
+// //https://myridius16-dev-ed.develop.lightning.force.com/lightning/page/home
+//   cy.visit('https://myridius16-dev-ed.develop.lightning.force.com/lightning/page/home');
 // });
