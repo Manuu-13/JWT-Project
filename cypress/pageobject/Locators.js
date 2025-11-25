4a2fc965f7b3e73eb7da2673b90a06a2f0ee1491
Akanksha Veer <akankshaveer28@gmail.com>
2025-11-25 18:06:09 +0530

    update Locators.js

--- cypress/pageobject/Locators.js
+++ cypress/pageobject/Locators.js
@@ -2,26 +2,44 @@ class Locators {
     username="//input[@id='username']";
     password="//input[@id='password']";
     login="//input[@id='Login']";
-    //click="//lightning-icon[@icon-name='utility:setup']";
-    newbtn="//a[@title='New']//div[text()='New']";
     Accname="//input[@name='Name']";
-    Accphone="//input[@name='Phone' and @type='text' and contains(@class, 'slds-input')]";
-    save="//button[@name='SaveEdit']"
-    converted="//a[@data-tab-name='converted' and .//span[text()='Converted']]";
     convertedStatus="//span[text()='Select Converted Status']/parent::button";
     convertbtn="//button[normalize-space()='Convert']";
-    lastname="//input[@name='lastName']";
     company="//input[@name='Company']";
-    phone="//input[@name='Phone']";
-    email="//input[@name='Email']";
-    save1="//button[@name='SaveEdit']"
-    converted1="//a[@data-tab-name='converted']";
+    converted="//a[@data-tab-name='converted']";
     selectconverted="//button[.//span[text()='Select Converted Status']]";
     convert="//button[normalize-space(text())='Convert']"
     opportunity="//h3[text()='Opportunity']/ancestor::div[contains(@class,'containerConvertedItem')]//div[@title='Account Name']/following-sibling::div//a[@data-refid='recordId']";
     account="//h3[text()='Account']/ancestor::div[contains(@class,'containerConvertedItem')]//a[contains(@href,'/lightning/r/001')]";
-    errorbox="//div[@data-name='BirthDate__c' and contains(@class,'slds-form-element__help')]";
+    errorbox="//div[@data-name='BirthDate__c' and contains(text(),'Please enter a valid Birthdate')]";
     birthday="//input[@name='BirthDate__c']";
+    convertedItems="div[class*='bodyConvertedItem']";
+    //--------------------------------------------
+    conSearchbtn="//button[contains(.,'Search')]";
+    conSearch="//input[@type='search' and @placeholder='Search...']";
+    //------------------------------------------
+    accTab="//a[@href='/lightning/o/Account/home' and @title='Accounts']//span[normalize-space()='Accounts']";
+    accSearch="//input[@placeholder='Search this list...' and contains(@class,'slds-input')]";
+    dropDown="//button[.//span[contains(text(),'Show more actions')]]";
+    accDelete="//a[@role='menuitem' and .//span[normalize-space()='Delete']]";
+    clear="//button[@data-element-id='searchClear']";
+    notFound="//h3[normalize-space()='Nothing to see here']";
     
+    //------------------common Locators---------------------------
+     //newbtn="//a[@role='button' and @title='New']//div[normalize-space()='New']";
+     newbtn="//a[.='New' or @title='New']";
+     save="//button[@name='SaveEdit' and normalize-space()='Save']";
+     website="//input[@name='Website']";
+     phone="//input[@name='Phone']";
+     email="//input[@name='Email' and @type='text' and @inputmode='email']";
+     lastname="//input[@name='lastName']";
+     firstname="//input[@name='firstName']";
+     Details="//a[normalize-space()='Details']";
+     phoneOnDetails="//span[text()='Phone']/ancestor::div[contains(@class,'slds-form-element')]//a";
+     websiteOnDetails="//span[text()='Website']/ancestor::div[contains(@class,'slds-form-element')]//a";
+     EditPhone="//button[@title='Edit Phone']";
+     delete="//span[normalize-space()='Delete' and contains(@class,'bBody')]";
+     //-----------------------------------------------------------
+
 }
 export const locator = new Locators();
